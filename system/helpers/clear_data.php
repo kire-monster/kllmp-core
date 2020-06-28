2cbfb46b26cb17ac565474ddecb37791fc33d93b
Erik Carrillo <hell-dxd@hotmail.com>
2020-06-28 14:50:51 -0500

    mejora de funcion clearData

--- system/helpers/clear_data.php
+++ system/helpers/clear_data.php
@@ -3,21 +3,70 @@
  * Limpia los caracteres
  */
 
-function clearData($dato){
-  $character = array(
-    0 => array("`", "&#96"),
-    1 => array('"', "&#34;"),
-    2 => array('%', "&#37"),
-    3 => array('<', '&lt;'),
-    4 => array('>', '&gt;'),
-    //5 => array("'", '&#8216;'),
-    5 => array("'", "\'"),
-    6 => array("\\", "\\"),
+function clearData($string)
+{
+  $string = trim($string);
+
+  $string = str_replace(
+    array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+    array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+    $string
   );
 
-  foreach ($character as $char) {
-    str_replace($char[0], $char[1], $dato);
-  }
+  $string = str_replace(
+    array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+    array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+    $string
+  );
 
-  return $dato;
+  $string = str_replace(
+    array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+    array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+    $string
+  );
+
+  $string = str_replace(
+    array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+    array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+    $string
+  );
+
+  $string = str_replace(
+    array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+    array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+    $string
+  );
+
+  $string = str_replace(
+    array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+    array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+    $string
+  );
+
+  $string = str_replace(
+    array('<script>','</script>', '<?php', '<?=', '?>'),
+    '',
+    $string
+  );
+
+  $string = str_replace(
+    array('"', "'", ),
+    array('&#34;','&#39;'),
+    $string
+  );
+
+  /*$string = str_replace(
+    array("\\", "¨", "º", "-", "~",
+         "#", "@", "|", "!", "\"",
+         "·", "$", "%", "&", "/",
+         "(", ")", "?", "'", "¡",
+         "¿", "[", "^", "<code>", "]",
+         "+", "}", "{", "¨", "´",
+         ">", "<", ";", ",", ":",
+         ".", " "),
+    '',
+    $string
+  );*/
+
+  return $string;
 }
